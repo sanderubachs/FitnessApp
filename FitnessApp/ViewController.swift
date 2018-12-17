c347391b21f071685d43ad4e0eaeebf364df2c00
issd <issd@macbook-pro-sander.local>
2018-12-17 09:44:31 +0100

    Initial Commit

---
+++ FitnessApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FitnessApp
+//
+//  Created by issd on 17/12/2018.
+//  Copyright © 2018 Fontys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
